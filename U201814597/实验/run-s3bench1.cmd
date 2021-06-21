c8cea3366895cfc9745356ed52fab60948b39b37
ruined404cjy <834325700@qq.com>
2021-06-21 17:31:04 +0800

    final commit

---
+++ U201814597/实验/run-s3bench1.cmd
@@ -0,0 +1,33 @@
+@rem -accessKey        Access Key
+@rem -accessSecret     Secret Key
+@rem -bucket=loadgen   Bucket for holding all test objects.
+@rem -endpoint=http://127.0.0.1:9000 Endpoint URL of object storage service being tested.
+@rem -numClients=8     Simulate 8 clients running concurrently.
+@rem -numSamples=256   Test with 256 objects.
+@rem -objectNamePrefix=loadgen Name prefix of test objects.
+@rem -objectSize=1024          Size of test objects(byte)
+@rem -verbose          Print latency for every request.
+
+@echo off
+@echo.>result1.txt
+
+set /a numClients = 8
+set /a objectSize = 0
+
+setlocal enabledelayedexpansion
+for /l %%i in (0,1,19) do ( 
+	set /a objectSize += 2048
+	s3bench.exe ^
+		-accessKey=hust ^
+		-accessSecret=hust_obs ^
+		-bucket=loadgen ^
+		-endpoint=http://127.0.0.1:9000 ^
+		-numClients=!numClients! ^
+		-numSamples=256 ^
+		-objectNamePrefix=loadgen ^
+		-objectSize=!objectSize! >> result1.txt	
+		echo !objectSize!	
+)
+
+
+
